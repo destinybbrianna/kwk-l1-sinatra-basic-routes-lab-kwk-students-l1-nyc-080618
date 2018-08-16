698505602c7553815daed2f6480839ee27187e57
destinybbrianna <destinybbrianna@gmail.com>
2018-08-16 14:21:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "Hello my name is destiny lol"
+   end
+    get '/hometown' do 
+    "My hometown is Bronx, New York "
+  end
+  get '/favorite-song' do 
+  "My favorite song is by Billie Eilish and called 'Watch'"
+  end
+  get '/favorite-food' do
+    "my favorite food is pasta and cake!"
+  end
+  get '/age' do
+    "i am 17 years old"
+  end 
 end
+
+
+ 
+
+ 
